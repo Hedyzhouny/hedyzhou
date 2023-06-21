68ac91c13226281f03c3ed0b38df6053e9c77c72
Hedyzhouny <hedyzhouny@gmail.com>
2023-06-21 18:00:38 -0400

    Initial Commit

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
 
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const cubeGeometry = new THREE.BoxGeometry(2, 0.5, 1)
+const cubeMaterial = new THREE.MeshBasicMaterial({
+  color: "#f16f93",
+});
+const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial)
+scene.add(roundMesh)
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
